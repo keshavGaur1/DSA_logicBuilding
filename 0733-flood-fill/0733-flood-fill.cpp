e70997ca7b86d8d55d54230029e5092f537b2568
Keshav Gaur <149561248+keshavGaur1@users.noreply.github.com>
2025-08-12 19:19:50 +0530

    Time: 0 ms (100%), Space: 18.5 MB (23.73%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    void traverse(vector<vector<int>>& image, int sr, int sc,
+                                  int newColor,int prevColor,vector<vector<int>>& visited,vector<vector<int>>& ans){
+        int n=image.size() , m=image[0].size() ;
+        visited[sr][sc]=1;
+        ans[sr][sc]=newColor;
+        
+        int rowArr[]={-1,0,+1,0};
+        int colArr[]={0,+1,0,-1};
+        
+        for(int i=0;i<4;i++){
+            int newR = sr+rowArr[i];
+            int newC = sc+colArr[i];
+            
+            if( newR>=0 && newR<n && newC>=0 && newC<m && visited[newR][newC]==0 && image[newR][newC]==prevColor ){
+                traverse(image,newR,newC,newColor,prevColor,visited,ans); 
+            }
+        }
+    }
+  
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int n=image.size() , m=image[0].size() ;
+        vector<vector<int>> ans(n,vector<int> (m,0));
+        vector<vector<int>> visited(n,vector<int> (m,0));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                ans[i][j] = image[i][j];
+            }
+        }
+        
+        traverse(image,sr,sc,newColor,image[sr][sc],visited,ans); 
+        
+        return ans ;
+    }
+};
