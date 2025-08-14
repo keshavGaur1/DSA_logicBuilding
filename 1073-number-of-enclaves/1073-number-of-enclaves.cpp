2a44a90e6324cf4c2d9517788e0bde2fd5220163
Keshav Gaur <149561248+keshavGaur1@users.noreply.github.com>
2025-08-14 14:51:29 +0530

    Time: 16 ms (19.01%), Space: 33.4 MB (59.32%) - LeetHub

---
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -0,0 +1,52 @@
+class Solution {
+void bfsMarking(int i,int j,vector<vector<int>>& visited,vector<vector<int>>& mat){
+      int n=mat.size() , m=mat[0].size() ;
+      visited[i][j]=1;
+      queue< pair<int,int> > q;
+      q.push( {i,j} );
+
+      int rowArr[] = {-1,0,+1,0};
+      int colArr[] = {0,+1,0,-1};
+
+      while( !q.empty() ){
+        int r=q.front().first;
+        int c=q.front().second;
+        q.pop();
+
+        for(int k=0;k<4;k++){
+          int newR=r+rowArr[k];
+          int newC=c+colArr[k];
+          
+          if(newR>=0 && newR<n && newC>=0 && newC<m && mat[newR][newC]==1 && visited[newR][newC]==0) {
+              visited[newR][newC]=1;
+              q.push( {newR,newC} );
+          }
+        }
+      }
+      
+    }
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n=grid.size() , m=grid[0].size() ;
+        vector<vector<int>> visited(n,vector<int> (m,0)) ;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if( ( i==0 || j==0 || i==n-1 || j==m-1) && grid[i][j]==1 && visited[i][j]==0 ){
+                    bfsMarking(i,j,visited,grid);
+                }
+            }
+        }
+
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if( grid[i][j]==1 && visited[i][j]==0 ){ // 1 hai and not visited h
+                    ans++;
+                }
+            }
+        }
+        
+        return ans ;
+    }
+};
