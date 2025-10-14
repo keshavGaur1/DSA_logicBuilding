0a6f719f86de482c9307cf2702c76bb6d1c5dce0
Keshav Gaur <149561248+keshavGaur1@users.noreply.github.com>
2025-10-14 22:29:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Nodes in BST Range/sum-of-nodes-in-bst-range.cpp
@@ -0,0 +1,28 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int nodeSum(Node* root, int l, int r) {
+        // code here
+        if(!root) return 0;
+        int ans = 0;
+        if(root -> data > l) ans += nodeSum(root -> left, l, r);
+        if(r > root -> data) ans += nodeSum(root -> right, l, r);
+        if(root -> data >= l && root -> data <= r) ans += root -> data;
+        return ans;
+    }
+};
