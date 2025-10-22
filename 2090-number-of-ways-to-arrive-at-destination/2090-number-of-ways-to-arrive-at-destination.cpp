b9d0b6440fab8b356f2d400973ef0f38488fa30e
Keshav Gaur <149561248+keshavGaur1@users.noreply.github.com>
2025-10-22 11:27:26 +0530

    Time: 12 ms (38.87%), Space: 37.4 MB (39.52%) - LeetHub

---
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        vector<vector< pair<int,int> >> adj(n);
+        for(auto i:roads){
+            adj[i[0]].push_back({i[1],i[2]});
+            adj[i[1]].push_back({i[0],i[2]});
+        }
+        
+        priority_queue< pair<long long,int> , vector<pair<long long,int>> , greater<pair<long long,int>> > pq;
+        // {dist,node}
+        vector<long long> dist(n,LLONG_MAX);
+        vector<long long> ways(n,0);
+        
+        pq.push({0,0});
+        dist[0]=0;
+        ways[0]=1;
+        
+        int mod = (int)(1e9+7);
+        
+        while(!pq.empty() ){
+            long long minDist = pq.top().first;
+            int u = pq.top().second;
+            pq.pop();
+            
+            for(auto it:adj[u]){
+                int v=it.first , wt=it.second;
+                // we have reached min distancr for the first time
+                if(minDist+wt < dist[v] ){
+                    dist[v]=minDist+wt;
+                    pq.push({minDist+wt,v});
+                    ways[v]=ways[u];
+                }
+                else if( minDist+wt == dist[v] ){ // we already reached this distance from another paths
+                    ways[v] = (ways[v]+ways[u])%mod ;
+                }
+            }
+        }
+        
+        return ways[n-1] % mod ;
+    }
+};
