05f53c73f2ae0f0a6e2a68826370da77beb84c2f
Keshav Gaur <149561248+keshavGaur1@users.noreply.github.com>
2025-10-28 19:11:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
+++ Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
@@ -1,47 +1,34 @@
 class Solution {
   public:
-    // Function to find distance of nearest 1 in the grid for each cell.
     vector<vector<int>> nearest(vector<vector<int>>& grid) {
-        // Code here
-        vector<vector<int>> ans = grid ;
-        int n=grid.size() , m=grid[0].size();
-        
-        vector<vector<int>> visited(n,vector<int> (m,0));
-        queue< pair< pair<int,int> ,int > > q; // { {i,j},nearest1 }
-        
+        // code here
+        int n=grid.size(), m=grid[0].size();
+        vector<vector<int>> ans(n, vector<int>(m,-1));
+        queue<pair<int,int>> bfsQ;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if( grid[i][j]==1 ){
-                    // ans mai already 1 hoga but fir bhi
+                if(grid[i][j]==1){ 
+                    bfsQ.push({i,j});
                     ans[i][j]=0;
-                    visited[i][j]=1;
-                    q.push({ {i,j},0 });
                 }
             }
         }
-        
-        while( !q.empty() ){
-            int i=q.front().first.first;
-            int j=q.front().first.second;
-            int nearest1=q.front().second;
-            q.pop();
-            
-            int rowArr[] = {-1,0,+1,0};
-            int colArr[] = {0,+1,0,-1};
-            
-            for(int k=0;k<4;k++){
-                int newR = i+rowArr[k];
-                int newC = j+colArr[k];
-                
-                if(newR>=0 && newR<n && newC>=0 && newC<m && visited[newR][newC]==0 ){
-                    ans[newR][newC]=nearest1+1;
-                    q.push({ {newR,newC},nearest1+1 });
-                    visited[newR][newC]=1;
-                    
+        int dx[4]={-1,0,1,0};
+        int dy[4]={0,-1,0,1};
+        while(!bfsQ.empty()){
+                int cx=bfsQ.front().first;
+                int cy=bfsQ.front().second;
+                bfsQ.pop();
+                for(int j=0;j<4;j++){
+                    int nx=cx+dx[j];
+                    int ny=cy+dy[j];
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && ans[nx][ny]==-1){
+                        ans[nx][ny]=ans[cx][cy]+1;
+                        bfsQ.push({nx,ny});
+                    }
                 }
-            }
         }
-        
         return ans;
+ 
     }
 };
