41329fb8506168af0fbf51bc3c73dc9194ffbccf
Keshav Gaur <149561248+keshavGaur1@users.noreply.github.com>
2025-10-30 14:35:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Replace O's with X's/replace-os-with-xs.cpp
+++ Difficulty: Medium/Replace O's with X's/replace-os-with-xs.cpp
@@ -1,48 +1,65 @@
-// User function Template for C++
-
 class Solution {
-  private:
-    void dfsMarking(int i,int j,vector<vector<int>>& visited,vector<vector<char>>& mat){
-      int n=mat.size() , m=mat[0].size() ;
-      visited[i][j]=1;
-      int rowArr[] = {-1,0,+1,0};
-      int colArr[] = {0,+1,0,-1};
-      
-      for(int k=0;k<4;k++){
-          int newR=i+rowArr[k];
-          int newC=j+colArr[k];
-          
-          if(newR>=0 && newR<n && newC>=0 && newC<m && mat[newR][newC]=='O' && visited[newR][newC]==0) {
-              dfsMarking(newR,newC,visited,mat);
-          }
-      }
-    }
   public:
-    vector<vector<char>> fill(vector<vector<char>>& mat) {
-        // code here
-        int n=mat.size() , m=mat[0].size() ;
-        vector<vector<int>> visited(n,vector<int> (m,0)) ;
+    void fill(vector<vector<char>>& grid) {
+        // Code here
+        int n=grid.size();
+        int m=grid[0].size();
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if( ( i==0 || j==0 || i==n-1 || j==m-1) && mat[i][j]=='O' && visited[i][j]==0 ){
-                    dfsMarking(i,j,visited,mat);
+        queue<pair<int, int>>q;
+        
+        for(int i=0; i<n; i++){
+            if(grid[i][0]=='O'){
+                grid[i][0]='C';
+                q.push({i, 0});
+            }
+            if(grid[i][m-1]=='O'){
+                grid[i][m-1]='C';
+                q.push({i, m-1});
+            }
+        }
+        
+        for(int j=0; j<m; j++){
+            if(grid[0][j]=='O'){
+                grid[0][j]='C';
+                q.push({0, j});
+            }
+            if(grid[n-1][j]=='O'){
+                grid[n-1][j]='C';
+                q.push({n-1, j});
+            }
+        }
+        
+        int dr[4]={0, 1, 0, -1};
+        int dc[4]={1, 0, -1, 0};
+        
+        while(!q.empty()){
+            auto it=q.front();
+            int r=it.first;
+            int c=it.second;
+            q.pop();
+            
+            for(int i=0; i<4; i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]=='O'){
+                    grid[nr][nc]='C';
+                    q.push({nr, nc});
                 }
             }
         }
         
-        vector<vector<char>> ans = mat;
+        for(int i=1; i<n-1; i++){
+            for(int j=1; j<m-1; j++){
+                if(grid[i][j]=='O') grid[i][j]='X';
+            }
+        }
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if( mat[i][j]=='X' || visited[i][j]==0 ){ // 'O' and visited through border 0
-                    ans[i][j]='X';
-                }
-                else{
-                    ans[i][j]='O';
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]=='C'){
+                    grid[i][j]='O';
                 }
             }
         }
-        return ans ;
     }
 };
