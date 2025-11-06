19e458b72f941d654c88d05fb1176f1e558037c8
Keshav Gaur <149561248+keshavGaur1@users.noreply.github.com>
2025-11-06 09:45:52 +0530

    Time: 0 ms (100%), Space: 19.4 MB (84.04%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,30 +1,69 @@
-class Solution {
-private:
-    void dfs(int node,vector<vector<int>>& isConnected,vector<int>& visited){
-        visited[node]=1;
-        // ans.push_back(node);
-        
-        // for every neighbour
-        for(int j=0;j<isConnected[node].size();j++ ){
-            if( isConnected[node][j]==1 && visited[j] == 0 ){  // not visited
-                dfs(j,isConnected,visited);
-            }
+class disjointSet
+{
+public:
+vector<int> parent , size;
+    disjointSet(int n)
+    {
+        // 1 based indexing (also works for 0 based indexing)
+        size.resize(n + 1, 1);
+
+        parent.resize(n + 1);
+
+        for (int i = 0; i < n + 1; i++)
+        {
+            parent[i] = i;
         }
     }
 
+    int findParent(int node)
+    {
+        // node aur uska parent same h , to whi parent h
+        if (node == parent[node])
+            return node;
+
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulPar_u = findParent(u);
+        int ulPar_v = findParent(v);
+
+        // agar u & v ke ultimate parent same hi h , to wo (u,v) phele se ek hi componant mai hai. No need to do anything in disjoint set D.S.
+        if (ulPar_u == ulPar_v)
+            return;
+
+        if (size[ulPar_u] < size[ulPar_v])
+        { // u ka size chota h , to connect u to v
+            parent[ulPar_u] = ulPar_v;
+            size[ulPar_v] += size[ulPar_u];
+        }
+        else
+        { // v ka size chota h , to connect v to u
+            parent[ulPar_v] = ulPar_u;
+            size[ulPar_u] += size[ulPar_v];
+        }
+    }
+};
+
+
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        vector<int> visited(n,0);
-        int cnt = 0;
-
-        // adj matrix to adj list
+        disjointSet ds(n);
 
         for(int i=0;i<n;i++){
-            if( visited[i] == 0 ){ // not visited
-                cnt++;
-                dfs(i,isConnected,visited);
+            for(int j=0;j<n;j++){
+                if( i!=j && isConnected[i][j]==1 ){
+                    ds.unionBySize(i,j);
+                }
             }
+        }
+
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if( ds.parent[i]==i ) cnt++;
         }
 
         return cnt;
